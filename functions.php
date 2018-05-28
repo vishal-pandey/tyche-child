9bab8c7df5611a7102882263aa97d1276a2bbbf3
vishal-pandey <pandeyvishal25@yahoo.com>
2018-05-28 08:15:49 +0530

    Automated commit script by vishal

--- functions.php
+++ functions.php
@@ -21,7 +21,4 @@ function woo_custom_hide_sales_flash()
 }
 
 // remove hover zoom
-function remove_image_zoom_support() {
-    remove_theme_support( 'wc-product-gallery-zoom' );
-}
-add_action( 'wp', 'remove_image_zoom_support', 100 );
+remove_theme_support( 'wc-product-gallery-zoom' );
